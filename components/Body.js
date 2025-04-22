84ed28b1364062732b3c74e5858e13d5c7c621dc
Eric Rannestad <ericrannestad@gmail.com>
2025-04-22 11:18:18 -0400

    commit

--- components/Body.js
+++ components/Body.js
@@ -5,10 +5,10 @@ export default function Body() {
     <div className="content">
        <section className='full-height justify-bottom small-padding '>
           <div className='spacer_2-3'></div>
-          <p><b>STRATA</b> (<i>There Are Always Tracks to Follow</i>) is an abstract representation of an excavation site based on previous archaeological digs near the site of the Harvard Indian College. It is a theoretical container of extracted earth, presented above the ground as an activation of public space and counterpoint to the mythologies of Harvard's origin. In the evening, this fabric structure doubles as a screening location for indigenous-directed and featured films. Taken together, it an installation concerned with our present moment, and with layers of sediment yet to be deposited.</p>
+          <p><b>STRATA</b> (<i>There Are Always Tracks to Follow</i>) is an abstract representation of an excavation site based on previous archaeological digs near the site of the Harvard Indian College. It is a theoretical container of extracted earth, presented above the ground as an activation of public space and counterpoint to the mythologies of Harvard's origin. In the evening, this fabric structure doubles as a screen for the projection of indigenous-directed and featured films. Taken together, it an installation concerned with our present moment, and with layers of sediment yet to be deposited.</p>
         </section>
         
-        <section id="site" className='full-height justify-bottom small-padding'>
+        <section id="site" className='full-height justify-bottom small-padding background-color-black'>
           <div className='spacer'></div>
           <div>
           <p>The Harvard Indian College, a two-story brick building, stood from 1633 until its dismantling in 1698. The Old College served as a multipurpose structure that included residences, as well as academic, dining, and kitchen spaces. The Indian College housed students from New England, Native American tribes, English students, and the first printing press in the American colonies. The Charter of 1630 dedicated Harvard “to the education of English and Indian Youth of this country in knowledge and godliness.</p>
@@ -19,7 +19,7 @@ export default function Body() {
           </div>
         </section>
 
-        <section id="events" className='full-height justify-bottom small-padding'>
+        <section id="events" className=' justify-bottom  background-color-black'>
           <div className='spacer'></div>
           <div>
           <p>We Still Live Here</p>
@@ -30,7 +30,7 @@ export default function Body() {
           </div>
         </section>
 
-        <section id="info" className='full-height justify-bottom small-padding'>
+        <section id="info" className=' justify-bottom  background-color-black'>
           <div className='spacer'></div>
           <div>
           <p>Ignacio Lira Montes</p>
