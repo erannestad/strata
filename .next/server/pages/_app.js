5d5b0509263868ac0720921af1e07f23ad52a7ed
Eric Rannestad <ericrannestad@gmail.com>
2025-05-01 03:01:34 -0400

    update w program

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,1 +1,68 @@
-(()=>{var r={};r.id=636,r.ids=[636],r.modules={2015:r=>{"use strict";r.exports=require("react")},7522:(r,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>u});var s=t(8732);function u({Component:r,pageProps:e}){return(0,s.jsx)(r,{...e})}t(2015),t(7757)},7757:()=>{},8732:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var t=e(e.s=7522);module.exports=t})();
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
+exports.modules = {
+
+/***/ "(pages-dir-node)/./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ App)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ \"react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../styles/style.css */ \"(pages-dir-node)/./styles/style.css\");\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_style_css__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\nfunction App({ Component, pageProps }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n        ...pageProps\n    }, void 0, false, {\n        fileName: \"/Users/ericrannestad/Sites/strata/pages/_app.js\",\n        lineNumber: 5,\n        columnNumber: 10\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHBhZ2VzLWRpci1ub2RlKS8uL3BhZ2VzL19hcHAuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7QUFBMEI7QUFDRztBQUVkLFNBQVNDLElBQUksRUFBRUMsU0FBUyxFQUFFQyxTQUFTLEVBQUU7SUFDbEQscUJBQU8sOERBQUNEO1FBQVcsR0FBR0MsU0FBUzs7Ozs7O0FBQ2pDIiwic291cmNlcyI6WyIvVXNlcnMvZXJpY3Jhbm5lc3RhZC9TaXRlcy9zdHJhdGEvcGFnZXMvX2FwcC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnO1xuaW1wb3J0ICcuLi9zdHlsZXMvc3R5bGUuY3NzJztcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gQXBwKHsgQ29tcG9uZW50LCBwYWdlUHJvcHMgfSkge1xuICByZXR1cm4gPENvbXBvbmVudCB7Li4ucGFnZVByb3BzfSAvPjtcbn1cbiJdLCJuYW1lcyI6WyJSZWFjdCIsIkFwcCIsIkNvbXBvbmVudCIsInBhZ2VQcm9wcyJdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(pages-dir-node)/./pages/_app.js\n");
+
+/***/ }),
+
+/***/ "(pages-dir-node)/./styles/style.css":
+/*!**************************!*\
+  !*** ./styles/style.css ***!
+  \**************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("(pages-dir-node)/./pages/_app.js"));
+module.exports = __webpack_exports__;
+
+})();
--- components/Body.js
+++ components/Body.js
@@ -3,30 +3,51 @@ import Canvas from '../components/Canvas'
 export default function Body() {
   return (
     <div className="content">
-       <section className='full-height justify-bottom small-padding '>
-          <div className='spacer_2-3'></div>
-          <p><b>STRATA</b> (<i>There Are Always Tracks to Follow</i>) is an abstract representation of an excavation site based on previous archaeological digs near the site of the Harvard Indian College. It is a theoretical container of extracted earth, presented above the ground as an activation of public space and counterpoint to the mythologies of Harvard's origin. In the evening, this fabric structure doubles as a screen for the projection of indigenous-directed and featured films. Taken together, it an installation concerned with our present moment, and with layers of sediment yet to be deposited.</p>
-        </section>
-        
-        <section id="site" className='full-height justify-bottom small-padding background-color-black'>
-          <div className='spacer'></div>
-          <div>
-          <p>The Harvard Indian College, a two-story brick building, stood from 1633 until its dismantling in 1698. The Old College served as a multipurpose structure that included residences, as well as academic, dining, and kitchen spaces. The Indian College housed students from New England, Native American tribes, English students, and the first printing press in the American colonies. The Charter of 1630 dedicated Harvard “to the education of English and Indian Youth of this country in knowledge and godliness.</p>
-
-          <p>Bricks of the Harvard Indian College, ceramics, old nails, and lead block type from the printing of the Eliot Bible, are examples of artifacts unearthed here. These objects, found in the depths of the excavation site, mark a history where the 17th century settler-colonial project interfaced with the region’s indigenous Massachusett, Wampanoag, and Nipmuck peoples through trade and exchange, but also violence, manipulation, and territorial dispossession.</p>
-
-          <p>In the evening, the structure transforms into a hybrid space of conversation and film screenings. Co-curated with the Harvard University Native American Program, these films present current indigenous stories.</p>
+       <section className='offset-header full-height background-color-black justify-bottom small-padding '>
+          <div id="events">
+            {/* <h2>Program</h2> */}
+            <div>
+              <div className='event'>
+                <span className='text-sm'>Screening</span>
+                <p><a href='https://www.pbs.org/independentlens/documentaries/we-still-live-here/'>We Still Live Here</a></p>
+                <div className='text-sm'>
+                  <p>Friday, May 2  at  2pm</p>
+                </div>
+                <p className='text-sm'>Directed by Anne Makepeace, the film tells the powerful story of the Wampanoag people and their efforts to reclaim their language and cultural identity. </p>
+              </div>
+              <div className='event'>
+                <span className='text-sm'>Watch Online</span>
+                <p><a href='https://upstanderproject.org/films/dawnland'>Dawnland</a></p>
+                <p className='text-sm'>Dawnland is the untold story of Indigenous child removal in the US through the nation's first-ever government-endorsed truth and reconciliation commission, which investigated the devastating impact of Maine’s child welfare practices on the Wabanaki people.</p>
+              </div>
+              <div className='event'>
+                <span className='text-sm'>Event</span>
+                <p>Stickball in the Yard</p>
+                <div className='text-sm'>
+                  <p>Thursday, May 1  at  8pm</p>
+                </div>                <p className='text-sm'>Come watch a game of casual stickball in the Harvard Yard! Organized by James Walkingstick.</p>
+              </div>
+              <div className='event'>
+                <span className='text-sm'>Watch Online</span>
+                <p>Yintah</p>
+                <div className='text-sm'>
+                  <p>Available on Netflix</p>
+                </div>                <p className='text-sm'>A focus on the impact on the lives of the Wet'suwet'en people who live on unceded territory, as they oppose the construction of multiple infrastructure projects</p>
+              </div>
+            </div>
           </div>
-        </section>
+          {/* <div className='spacer_2-3'></div> */}
+            <p><b>STRATA</b> references the physical form of recent archeologic dig sites near the location of the 17th century Indian College. The installation takes a theoretical container of extracted earth and presents it above ground as an activation of public space and as a counterpoint to the mythologies of Harvard's origin. In the evening, this geotextile structure doubles as a screening space for various Indigenous-directed and featured films, co-curated with the Harvard University Native American Program.
+            </p>
 
-        <section id="events" className=' justify-bottom  background-color-black'>
+        </section>
+        <section id="site" className='full-height justify-bottom small-padding'></section>
+        <section id="site" className=' small-padding background-color-black'>
           <div className='spacer'></div>
           <div>
-          <p>We Still Live Here</p>
+          <p>The construction of the Indian College was shaped by Harvard’s early financial hardship and by the Charter of 1650, which committed to "the education of English and Indian youth.” Recent archaeological digs uncovered brick from the structure; ceramics, pipes, and other items offered insights into the lives of the resident English and Native American students; as well as lead movable type from the production of the first Bible translated into an Indigenous North American language. These artifacts were found below more recent layers of soil marked by contemporary life: petrochemical deposits, modern currency, and other traces of the Anthropocene. </p>
 
-          <p>Dawnland</p>
-
-          <p>Yintah</p>
+          <p>These material layers evidence a continuum of interconnected processes that began as a settler-colonial project, and unfolded across time and space into our present moment—transformed, but embedded. This ephemeral architectural installation offers a visual realignment of the Old Yard toward Harvard’s complicated relationship with the Indigenous people of North America and founding institutional motivations. The project brings together students, faculty, artists, and the local community to engage with these temporalities, layers, and evolving stories.</p>
           </div>
         </section>
 
