6e3db956816acbdca80aae8c4a03ed7750771d8c
Eric Rannestad <ericrannestad@gmail.com>
2026-03-05 16:58:44 -0500

    update

--- components/Layout.js
+++ components/Layout.js
@@ -6,9 +6,6 @@ export default function Layout({ children }) {
       <Head>
         <title>STRATA</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
       </Head>
       <>{children}</>
     </>
